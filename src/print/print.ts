5d01bd55409aaf8de90672447ed4b81dde78ac61
Kevin Simon <kevin.simon@quesoft.hu>
2023-08-22 10:55:41 +0200

    fix

--- src/print/print.ts
+++ src/print/print.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { promises as fs } from "fs";
+import fs from "fs/promises";
 import fsBase from "fs";
 import execAsync from "../utils/exec-file-async";
 import fixPathForAsarUnpack from "../utils/electron-util";
